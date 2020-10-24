350e4ac584782800544b66917fb3396b87e94692
Carlos Luiz Marinho da Silva Junior <cljunior@brasilseg.com.br>
2020-10-24 17:19:39 -0300

    configurado react-router-dom para rotas

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+
+const Home = () => <div>Home</div>;
+const SearchResults = () => <div>Search Results</div>
 
 const App = () => {
     return (
-        <div>App</div>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/search" component={SearchResults} />
+            </Switch>
+        </BrowserRouter>
     )
 }
 
