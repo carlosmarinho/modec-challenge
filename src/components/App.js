86ae9f19ed6decfa49d361af0043df8ccea90466
Carlos Luiz Marinho da Silva Junior <cljunior@brasilseg.com.br>
2020-10-24 17:34:08 -0300

    adicionado header e instalado styled components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import Header from 'components/Header';
 import Home from 'pages/home';
 import SearchResults from 'pages/search';
 
 const App = () => {
     return (
+        <>
+        <Header />
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/search" component={SearchResults} />
             </Switch>
         </BrowserRouter>
+        </>
     )
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const HeaderWrapper = styled.div`
+`
+
+const Header = () => {
+    return <HeaderWrapper>Header</HeaderWrapper>
+}
+
+export default Header;
