8817598fbb42b0267dbc071daa087c2569645c7e
Carlos Luiz Marinho da Silva Junior <cljunior@brasilseg.com.br>
2020-10-24 17:58:26 -0300

    iniciado o layout do header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,31 @@ import styled from 'styled-components';
 const HeaderWrapper = styled.div`
     background-color: #ddd;
     width: 100%;
+    height: 60px;
+    margin-bottom: 20px;
+`
+
+const Logo = styled.div`
+    height: 100%; 
+    float: left; 
+    width: 40%; 
+    background-color: red;
+`
+
+const LoginArea = styled.div`
+    height: 100%; 
+    float: right; 
+    width: 50%; 
+    background-color: green;
 `
 
 const Header = () => {
-    return <HeaderWrapper>Header</HeaderWrapper>
+    return (
+        <HeaderWrapper>
+            <Logo><img src="" alt="Logo"/></Logo>
+            <LoginArea>Login</LoginArea>
+        </HeaderWrapper>
+    )
 }
 
 export default Header;
