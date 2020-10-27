d3f7786ccb439e0e26b81533d3137d00185bb449
Carlos Luiz Marinho da Silva Junior <cljunior@brasilseg.com.br>
2020-10-27 18:38:56 -0300

    configurado o middleware redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore,  } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import App from 'components/App';
 
 const store = createStore(
-    () => {}   
+    () => {},
+    applyMiddleware(reduxThunk)
 )
 
 ReactDOM.render(
